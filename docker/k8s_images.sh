631d9afb5252e9a62090b948b80789911d3ad92c
donald.chen <liujin.chen@qq.com>
2017-04-11 15:36:54 +0800

    k8s images

---
+++ docker/k8s_images.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+images=(\ 
+	kube-apiserver-amd64:v1.6.0 \ 
+	kube-controller-manager-amd64:v1.6.0 \ 
+	kube-scheduler-amd64:v1.6.0 \ 
+	kube-proxy-amd64:v1.6.0 \ 
+	etcd-amd64:3.0.17 \ 
+	pause-amd64:3.0 \ 
+	k8s-dns-sidecar-amd64:1.14.1 \ 
+	k8s-dns-kube-dns-amd64:1.14.1 \ 
+	k8s-dns-dnsmasq-nanny-amd64:1.14.1 \ 
+	kubernetes-dashboard-amd64:v1.6.0 \ 
+	nginx-ingress-controller:0.9.0-beta.3)
+for imageName in ${images[@]} ; do
+  docker pull 	docker.io/chenliujin/$imageName
+  docker tag 	docker.io/chenliujin/$imageName gcr.io/google_containers/$imageName
+  docker rmi 	docker.io/chenliujin/$imageName
+done
