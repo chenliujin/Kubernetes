5c48a595e33afff73bc76f221ce23836bec664bc
chenliujin <liujin.chen@qq.com>
2017-06-22 17:38:37 +0800

    remove

--- efk/weixin_cp_order_new.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run \
-	-it \
-	--rm \
-	-v /usr/share/java:/usr/share/java \
-	-v /opt/logstash/conf:/opt/logstash/conf \
-	logstash:5.4.2 \
-	logstash -f /opt/logstash/conf/weixin_cp_order_new.conf
