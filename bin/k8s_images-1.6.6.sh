93fa9469cebb2944745206c5c2d3ca8dd2af8f5b
chenliujin <liujin.chen@qq.com>
2017-06-27 16:31:30 +0800

    k8s_images-1.6.6

--- bin/k8s_images-1.6.6.sh
+++ bin/k8s_images-1.6.6.sh
@@ -3,7 +3,12 @@ images=(\
 	kube-apiserver-amd64:v1.6.6 \ 
 	kube-controller-manager-amd64:v1.6.6 \ 
 	kube-scheduler-amd64:v1.6.6 \ 
-	kube-proxy-amd64:v1.6.6 
+	kube-proxy-amd64:v1.6.6 \ 
+	etcd-amd64:3.0.17 \ 
+	pause-amd64:3.0 \ 
+	k8s-dns-sidecar-amd64:1.14.2 \ 
+	k8s-dns-kube-dns-amd64:1.14.2 \ 
+	k8s-dns-dnsmasq-nanny-amd64:1.14.2
 	)
 for imageName in ${images[@]} ; do
 	docker pull 	docker.io/chenliujin/$imageName
