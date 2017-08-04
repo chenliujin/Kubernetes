53db46e8e9081c5c8f360747362187cf10e50d8e
chenliujin <liujin.chen@qq.com>
2017-08-04 12:30:40 +0800

    kubeadm.sh

--- kubeadm/1.7.3/kubeadm.sh
+++ kubeadm/1.7.3/kubeadm.sh
@@ -4,7 +4,7 @@ yum install -y \
 	socat \
 	kubeadm-1.7.3-1.x86_64.rpm \
 	kubectl-1.7.3-1.x86_64.rpm \
-	kubectl-1.7.3-1.x86_64.rpm \
+	kubelet-1.7.3-1.x86_64.rpm \
 	kubernetes-cni-0.5.1-0.x86_64.rpm
 
 systemctl enable kubelet
