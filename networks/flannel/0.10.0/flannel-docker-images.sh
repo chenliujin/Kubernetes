03e5b745463b592d4a13b814faa84594a5d29f50
chenliujin <liujin.chen@qq.com>
2018-04-14 19:43:10 +0800

    flannel-docker-images.sh

---
+++ networks/flannel/0.10.0/flannel-docker-images.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+registry=$1
+version=v0.10.0-amd64
+
+if [ -z $registry ]; then
+	echo 'pls input registry server to push'
+	exit
+fi	
+
+docker pull 	docker.io/chenliujin/flannel:$version
+docker tag 	docker.io/chenliujin/flannel:$version  $registry/flannel:$version
+docker rmi 	docker.io/chenliujin/flannel:$version
+
+docker push $registry/flannel:$version
+
