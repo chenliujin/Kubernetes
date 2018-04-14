434f0f2535db432c17ca2dceabbc1a414f81bbaa
chenliujin <liujin.chen@qq.com>
2018-04-14 21:02:24 +0800

    ingress-docker-images.sh

--- ingress/examples/rbac/nginx/ingress-docker-images.sh
+++ ingress/examples/rbac/nginx/ingress-docker-images.sh
@@ -3,19 +3,19 @@
 registry=$1
 
 if [ -z $registry ]; then
-	echo 'pls input registry server to push'
-	exit
+  echo 'pls input registry server to push'
+  exit
 fi	
 
 
 images=(\ 
-	nginx-ingress-controller:0.9.0-beta.11 \ 
-	defaultbackend:1.0
-	)
+nginx-ingress-controller:0.9.0-beta.11 \ 
+defaultbackend:1.0
+)
 
 for imageName in ${images[@]} ; do
-	docker pull 	docker.io/chenliujin/$imageName
-	docker tag 	docker.io/chenliujin/$imageName $registry/google_containers/$imageName
-	docker rmi 	docker.io/chenliujin/$imageName
-	docker push $registry/google_containers/$imageName
+  docker pull docker.io/chenliujin/$imageName
+  docker tag 	docker.io/chenliujin/$imageName $registry/google_containers/$imageName
+  docker rmi 	docker.io/chenliujin/$imageName
+  docker push $registry/google_containers/$imageName
 done
